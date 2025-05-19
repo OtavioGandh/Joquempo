e9e09cab4cf102cef698a007aa672dc53f6bfe1d
otavi <otaviogandhi@gmail.com>
2025-05-19 16:10:36 -0300

    Colocando a funçao on click em cada um

--- app/src/main/java/com/example/joquempo/MainActivity.java
+++ app/src/main/java/com/example/joquempo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.joquempo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void selecionarPedra(View view){
+
+        verificarGanhador("pedra");
+    }
+    public void selecionarPapel(View view){
+
+        verificarGanhador("Papel");
+    }
+    public void selecionarTesoura(View view){
+
+        verificarGanhador("Tesoura");
+    }
+
+    private void verificarGanhador(String escolhaUsuario){
+
+        System.out.println("Item clicado "+escolhaUsuario);
     }
 }
